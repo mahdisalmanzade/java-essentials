bd7233173db63d59acfd8bdbbc213d388ab4cc40
Mahdi Salmanizadehgan <mahdisalmanizadehgan@gmail.com>
2025-05-29 14:12:10 +0330

    Demonstrating reference types
    feat: Demonstrating the idea that reference types are passed by coping the reference and any manipulation in the target method will affect the caller method

--- src/main/java/com/codewithmahdi/PassByValVsRef.java
+++ src/main/java/com/codewithmahdi/PassByValVsRef.java
@@ -1,12 +1,33 @@
 package com.codewithmahdi;
 
+import java.awt.*;
+
 public class PassByValVsRef {
     public static void main(String[] args) {
         int x = 1;
-        int val = incrementByOne(x);
+        int val = incrementByOne(x); // a cope of the x value is passed(passed by value)
         System.out.println(x);
         System.out.println(val);
+
+        Point point = new Point(100, 100);
+        // before passing the reference to a method
+        System.out.println(point);
+
+        // after passing the reference and manipulating it
+        changePoint(point); // a copy of the reference is passed(still passed by value, not by reference)
+        System.out.println(point); // the original object is affected
     }
+
+    private static void changePoint(Point point) {
+        /*
+          no longer modifies the original object in the main method
+          because we are assigning a new object to the reference parameter
+        */
+        point = new Point(200, 200);
+        point.x = 10;
+        point.y = 10;
+    }
+
 
     static int incrementByOne(int value) {
        return value + 1;
